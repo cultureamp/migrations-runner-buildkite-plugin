cultureamp/ecs-task-runner-buildkite-plugin

feat: plugin.RUN is the entrypoint of the business logic

src/plugin/config.go
@@ -5,7 +5,8 @@ import (
 )
 
 type Config struct {
-	Message string `envconfig:"MESSAGE" required:"true"`
+	ParameterName string `split_words:"true" required:"true"`
+	Script        string `split_words:"true" required:"true"`
 }
 
 type EnvironmentConfigFetcher struct {

cultureamp/migrations-runner-buildkite-plugin

src/plugin/config.go
@@ -5,7 +5,8 @@ import (
 )
 
 type Config struct {
-	Message string `envconfig:"MESSAGE" required:"true"`
+	ParameterName string `required:"true" split_words:"true"`
+	Script        string `required:"true" split_words:"true"`
 }
 
 type EnvironmentConfigFetcher struct {