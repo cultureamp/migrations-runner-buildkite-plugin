cultureamp/ecs-task-runner-buildkite-plugin

feat: add TaskRunner code
This change is a naive golang translation of the `TaskRunner` code from
Murmur (refer to notes). The aim of this commit is to get the initial
code into the repo and start building on it from there.

Notes:
- TaskRunner permalink: https://github.com/cultureamp/murmur/blob/260f8936d59aaa6074c6fc6f31b935e011bda9f6/ops/migrations-runner/lib/TaskRunner.ts
- migrations-runner permalink: https://github.com/cultureamp/murmur/tree/078d0154b1215f226df5d3d081dfd131a8ae898a/ops/migrations-runner

src/aws/cloudwatch_test.go
@@ -1,0 +1,99 @@
+package aws
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs/types"
+	"github.com/stretchr/testify/assert"
+)
+
+type mockCloudwatchLogsClient struct {
+	mockGetLogEvents func(ctx context.Context, params *cloudwatchlogs.GetLogEventsInput, optFns ...func(*cloudwatchlogs.Options)) (*cloudwatchlogs.GetLogEventsOutput, error)
+}
+
+func (m mockCloudwatchLogsClient) GetLogEvents(ctx context.Context, params *cloudwatchlogs.GetLogEventsInput, optFns ...func(*cloudwatchlogs.Options)) (*cloudwatchlogs.GetLogEventsOutput, error) {
+	return m.mockGetLogEvents(ctx, params, optFns...)
+}
+
+func TestRetrieveLogs(t *testing.T) {
+	input := LogDetails{
+		logGroupName:  "test-group",
+		logStreamName: "test-stream/test-container/07cc583696bd44e0be450bff7314ddaf",
+	}
+
+	events := []types.OutputLogEvent{
+		{
+			IngestionTime: aws.Int64(0),
+			Message:       aws.String("beans have been detected in the system"),
+			Timestamp:     aws.Int64(0),
+		},
+		{
+			IngestionTime: aws.Int64(1),
+			Message:       aws.String("beans have been removed from the system"),
+			Timestamp:     aws.Int64(1),
+		},
+	}
+
+	positiveTests := []struct {
+		name     string
+		input    LogDetails
+		client   mockCloudwatchLogsClient
+		expected []types.OutputLogEvent
+	}{
+		{
+			// This is a regression test to ensure the function signature remains the same
+			name:  "given a valid LogDetails input, return the events of the log stream",
+			input: input,
+			client: mockCloudwatchLogsClient{
+				mockGetLogEvents: func(ctx context.Context, params *cloudwatchlogs.GetLogEventsInput, optFns ...func(*cloudwatchlogs.Options)) (*cloudwatchlogs.GetLogEventsOutput, error) {
+					return &cloudwatchlogs.GetLogEventsOutput{Events: events}, nil
+				},
+			},
+			expected: events,
+		},
+	}
+
+	for _, tc := range positiveTests {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := RetrieveLogs(context.TODO(), tc.client, tc.input)
+
+			t.Logf("result: %v", result)
+			t.Logf("expected: %v", tc.expected)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+
+	negativeTests := []struct {
+		name     string
+		input    LogDetails
+		client   mockCloudwatchLogsClient
+		expected []types.OutputLogEvent
+	}{
+		{
+			name:  "when the underlying cloudwatch client experiences an error, return it in the function ",
+			input: input,
+			client: mockCloudwatchLogsClient{
+				mockGetLogEvents: func(ctx context.Context, params *cloudwatchlogs.GetLogEventsInput, optFns ...func(*cloudwatchlogs.Options)) (*cloudwatchlogs.GetLogEventsOutput, error) {
+					return &cloudwatchlogs.GetLogEventsOutput{}, errors.New("generic cloudwatch error")
+				},
+			},
+			expected: []types.OutputLogEvent{},
+		},
+	}
+
+	for _, tc := range negativeTests {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := RetrieveLogs(context.TODO(), tc.client, tc.input)
+
+			t.Logf("result: %v", result)
+			t.Logf("expected: %v", tc.expected)
+			assert.Error(t, err)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}

src/aws/ecs.go
@@ -1,0 +1,134 @@
+package aws
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ecs"
+	"github.com/aws/aws-sdk-go-v2/service/ecs/types"
+)
+
+// internal interface for ecs
+type EcsClientAPI interface {
+	RunTask(ctx context.Context, params *ecs.RunTaskInput, optFns ...func(*ecs.Options)) (*ecs.RunTaskOutput, error)
+	DescribeTasks(ctx context.Context, params *ecs.DescribeTasksInput, optFns ...func(*ecs.Options)) (*ecs.DescribeTasksOutput, error)
+	DescribeTaskDefinition(ctx context.Context, params *ecs.DescribeTaskDefinitionInput, optFns ...func(*ecs.Options)) (*ecs.DescribeTaskDefinitionOutput, error)
+}
+
+type ecsWaiterAPI interface {
+	WaitForOutput(ctx context.Context, params *ecs.DescribeTasksInput, maxWaitDur time.Duration, optFns ...func(*ecs.TasksStoppedWaiterOptions)) (*ecs.DescribeTasksOutput, error)
+}
+
+func SubmitTask(ctx context.Context, ecsAPI EcsClientAPI, input *TaskRunnerConfiguration) (string, error) {
+	response, err := ecsAPI.RunTask(ctx, &ecs.RunTaskInput{
+		Cluster:    &input.Cluster,
+		LaunchType: "FARGATE",
+		Overrides: &types.TaskOverride{
+			ContainerOverrides: []types.ContainerOverride{
+				{
+					Name:    aws.String("migrations-runner"),
+					Command: input.Command,
+				},
+			},
+		},
+		TaskDefinition: &input.TaskDefinitionArn,
+		NetworkConfiguration: &types.NetworkConfiguration{
+			AwsvpcConfiguration: &types.AwsVpcConfiguration{
+				Subnets:        input.SubnetIds,
+				SecurityGroups: input.SecurityGroupIds,
+			},
+		},
+	})
+	if err != nil {
+		return "", err
+	}
+
+	if response.Tasks[0].TaskArn == nil {
+		responseJSON, _ := json.Marshal(response)
+		return "", fmt.Errorf("ecs:RunTask response contains no TaskArn: %v", string(responseJSON))
+	}
+
+	// this is working on the assumption that only one task is returned
+	return *response.Tasks[0].TaskArn, nil
+}
+
+func WaitForCompletion(ctx context.Context, waiter ecsWaiterAPI, taskArn string) (*ecs.DescribeTasksOutput, error) {
+	cluster := ClusterFromTaskArn(taskArn)
+
+	maxWaitDuration := 15 * time.Minute
+	result, err := waiter.WaitForOutput(ctx, &ecs.DescribeTasksInput{
+		Cluster: aws.String(cluster),
+		Tasks:   []string{taskArn},
+	}, maxWaitDuration)
+
+	// the `DescribeTasksOutput` struct is returned even if there is an error. Counterintuitively, it happens to include failure information
+	// which we may want to surface from the `Failures` struct field
+	if err != nil {
+		return result, err
+	}
+
+	// In a successful scenario, we should have a `tasks` slice with a single element
+	return result, nil
+
+}
+
+func ClusterFromTaskArn(arn string) string {
+	parts := strings.Split(arn, "/")
+	return parts[len(parts)-2]
+}
+
+func TaskIdFromArn(taskArn string) string {
+	parts := strings.Split(taskArn, "/")
+	return parts[len(parts)-1]
+}
+
+// Describes a single task from a cluster using the native `ecs:DescribeTasks` API.
+// The taskArn is the task of interest to be described
+func DescribeTask(ctx context.Context, ecsClientAPI EcsClientAPI, taskArn string) (types.Task, error) {
+	cluster := ClusterFromTaskArn(taskArn)
+	response, err := ecsClientAPI.DescribeTasks(ctx, &ecs.DescribeTasksInput{
+		Tasks:   []string{taskArn},
+		Cluster: &cluster,
+	})
+
+	if err != nil {
+		return types.Task{}, err
+	}
+
+	// Given the input of `DescribeTasks` we should only be getting one Task back
+	return response.Tasks[0], nil
+}
+
+// Acquires LogStream details for given ECS Task
+func FindLogStreamFromTask(ctx context.Context, ecsClientApi EcsClientAPI, task types.Task) (LogDetails, error) {
+	response, err := ecsClientApi.DescribeTaskDefinition(ctx, &ecs.DescribeTaskDefinitionInput{
+		TaskDefinition: task.TaskDefinitionArn,
+	})
+	if err != nil {
+		return LogDetails{}, err
+	}
+
+	// TODO: This was originally part of the if statement below, but it was moved out to avoid a nil pointer dereference when getting the `logGroupName` and `streamPrefix` values
+	if len(response.TaskDefinition.ContainerDefinitions) == 0 {
+		return LogDetails{}, fmt.Errorf("ecs:DescribeTaskDefinition response is missing ContainerDefinitions data: %v", response)
+	}
+
+	container := response.TaskDefinition.ContainerDefinitions[0] // assume first container is the application container
+	logGroupName := container.LogConfiguration.Options["awslogs-group"]
+	//NOTE: Takes the format: prefix-name/container-name/ecs-task-id
+	streamPrefix := container.LogConfiguration.Options["awslogs-stream-prefix"]
+
+	// We need the logGroupName, streamPrefix, and a container name to be able to produce a FindLogStreamOutput in full
+	if logGroupName == "" || streamPrefix == "" {
+		return LogDetails{}, fmt.Errorf("ecs:DescribeTaskDefinition response does not conftain required logging configuration: %v", response)
+	}
+
+	return LogDetails{
+		logGroupName:  logGroupName,
+		logStreamName: fmt.Sprintf("%s/%s/%s", streamPrefix, *container.Name, TaskIdFromArn(*task.TaskArn)),
+	}, nil
+}

src/aws/ssm_test.go
@@ -1,0 +1,82 @@
+package aws
+
+import (
+	"context"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"github.com/aws/aws-sdk-go-v2/service/ssm/types"
+	"github.com/stretchr/testify/assert"
+)
+
+type mockGetParameter func(ctx context.Context, params *ssm.GetParameterInput, optFns ...func(*ssm.Options)) (*ssm.GetParameterOutput, error)
+
+func (m mockGetParameter) GetParameter(ctx context.Context, params *ssm.GetParameterInput, optFns ...func(*ssm.Options)) (*ssm.GetParameterOutput, error) {
+	return m(ctx, params, optFns...)
+}
+
+func TestRetrieveConfiguration(t *testing.T) {
+	// JSON strings that would be returned from the SSM parameter store
+	testTaskConfig1 := `{"cluster": "test-cluster","command": ["echo", "hello"],"subnetIds": ["subnet-123456"],"securityGroupIds": ["sg-123456"],"taskDefinitionArn": "arn:aws:ecs:us-west-2:123456789012:task-definition/test-task-1"}`
+	testTaskConfig2 := `{"cluster": "test-cluster","command": ["echo", "byebye"],"subnetIds": ["subnet-654321"],"securityGroupIds": ["sg-654321"],"taskDefinitionArn": "arn:aws:ecs:us-west-2:123456789012:task-definition/test-task-2"}`
+
+	mockedClient1 := mockGetParameter(func(ctx context.Context, params *ssm.GetParameterInput, optFns ...func(*ssm.Options)) (*ssm.GetParameterOutput, error) {
+		return &ssm.GetParameterOutput{
+			Parameter: &types.Parameter{
+				Value: aws.String(testTaskConfig1),
+			},
+		}, nil
+	})
+	mockedClient2 := mockGetParameter(func(ctx context.Context, params *ssm.GetParameterInput, optFns ...func(*ssm.Options)) (*ssm.GetParameterOutput, error) {
+		return &ssm.GetParameterOutput{
+			Parameter: &types.Parameter{
+				Value: aws.String(testTaskConfig2),
+			},
+		}, nil
+	})
+
+	// Running 2 different test cases to validate that the JSON payload unmarshals into the TaskRunnerConfiguration struct
+	tests := []struct {
+		name     string
+		input    string
+		client   ssmAPI
+		expected *TaskRunnerConfiguration
+	}{
+		{
+			name:   "given the parameter name test-parameter-name-1, it should return the configuration",
+			input:  "test-parameter-name-1",
+			client: mockedClient1,
+			expected: &TaskRunnerConfiguration{
+				Cluster:           "test-cluster",
+				Command:           []string{"echo", "hello"},
+				SecurityGroupIds:  []string{"sg-123456"},
+				SubnetIds:         []string{"subnet-123456"},
+				TaskDefinitionArn: "arn:aws:ecs:us-west-2:123456789012:task-definition/test-task-1",
+			},
+		},
+		{
+			name:   "given the parameter name test-parameter-name-2, it should return the configuration",
+			input:  "test-parameter-name-2",
+			client: mockedClient2,
+			expected: &TaskRunnerConfiguration{
+				Cluster:           "test-cluster",
+				Command:           []string{"echo", "byebye"},
+				SecurityGroupIds:  []string{"sg-654321"},
+				SubnetIds:         []string{"subnet-654321"},
+				TaskDefinitionArn: "arn:aws:ecs:us-west-2:123456789012:task-definition/test-task-2",
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := RetrieveConfiguration(context.TODO(), tc.client, tc.input)
+
+			t.Logf("result: %v", result)
+			t.Logf("expected: %v", tc.expected)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}

cultureamp/migrations-runner-buildkite-plugin

src/aws/cloudwatch_test.go
@@ -1,0 +1,100 @@
+package aws
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type mockCloudwatchLogsClient struct {
+	mockGetLogEvents func(ctx context.Context, params *cloudwatchlogs.GetLogEventsInput, optFns ...func(*cloudwatchlogs.Options)) (*cloudwatchlogs.GetLogEventsOutput, error)
+}
+
+func (m mockCloudwatchLogsClient) GetLogEvents(ctx context.Context, params *cloudwatchlogs.GetLogEventsInput, optFns ...func(*cloudwatchlogs.Options)) (*cloudwatchlogs.GetLogEventsOutput, error) {
+	return m.mockGetLogEvents(ctx, params, optFns...)
+}
+
+func TestRetrieveLogs(t *testing.T) {
+	input := LogDetails{
+		logGroupName:  "test-group",
+		logStreamName: "test-stream/test-container/07cc583696bd44e0be450bff7314ddaf",
+	}
+
+	events := []types.OutputLogEvent{
+		{
+			IngestionTime: aws.Int64(0),
+			Message:       aws.String("beans have been detected in the system"),
+			Timestamp:     aws.Int64(0),
+		},
+		{
+			IngestionTime: aws.Int64(1),
+			Message:       aws.String("beans have been removed from the system"),
+			Timestamp:     aws.Int64(1),
+		},
+	}
+
+	positiveTests := []struct {
+		name     string
+		input    LogDetails
+		client   mockCloudwatchLogsClient
+		expected []types.OutputLogEvent
+	}{
+		{
+			// This is a regression test to ensure the function signature remains the same
+			name:  "given a valid LogDetails input, return the events of the log stream",
+			input: input,
+			client: mockCloudwatchLogsClient{
+				mockGetLogEvents: func(ctx context.Context, params *cloudwatchlogs.GetLogEventsInput, optFns ...func(*cloudwatchlogs.Options)) (*cloudwatchlogs.GetLogEventsOutput, error) {
+					return &cloudwatchlogs.GetLogEventsOutput{Events: events}, nil
+				},
+			},
+			expected: events,
+		},
+	}
+
+	for _, tc := range positiveTests {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := RetrieveLogs(context.TODO(), tc.client, tc.input)
+
+			t.Logf("result: %v", result)
+			t.Logf("expected: %v", tc.expected)
+			require.NoError(t, err)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+
+	negativeTests := []struct {
+		name     string
+		input    LogDetails
+		client   mockCloudwatchLogsClient
+		expected []types.OutputLogEvent
+	}{
+		{
+			name:  "when the underlying cloudwatch client experiences an error, return it in the function ",
+			input: input,
+			client: mockCloudwatchLogsClient{
+				mockGetLogEvents: func(ctx context.Context, params *cloudwatchlogs.GetLogEventsInput, optFns ...func(*cloudwatchlogs.Options)) (*cloudwatchlogs.GetLogEventsOutput, error) {
+					return &cloudwatchlogs.GetLogEventsOutput{}, errors.New("generic cloudwatch error")
+				},
+			},
+			expected: []types.OutputLogEvent{},
+		},
+	}
+
+	for _, tc := range negativeTests {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := RetrieveLogs(context.TODO(), tc.client, tc.input)
+
+			t.Logf("result: %v", result)
+			t.Logf("expected: %v", tc.expected)
+			require.Error(t, err)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}

src/aws/ecs.go
@@ -1,0 +1,136 @@
+package aws
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ecs"
+	"github.com/aws/aws-sdk-go-v2/service/ecs/types"
+)
+
+// internal interface for ecs
+type EcsClientAPI interface {
+	RunTask(ctx context.Context, params *ecs.RunTaskInput, optFns ...func(*ecs.Options)) (*ecs.RunTaskOutput, error)
+	DescribeTasks(ctx context.Context, params *ecs.DescribeTasksInput, optFns ...func(*ecs.Options)) (*ecs.DescribeTasksOutput, error)
+	DescribeTaskDefinition(ctx context.Context, params *ecs.DescribeTaskDefinitionInput, optFns ...func(*ecs.Options)) (*ecs.DescribeTaskDefinitionOutput, error)
+}
+
+type ecsWaiterAPI interface {
+	WaitForOutput(ctx context.Context, params *ecs.DescribeTasksInput, maxWaitDur time.Duration, optFns ...func(*ecs.TasksStoppedWaiterOptions)) (*ecs.DescribeTasksOutput, error)
+}
+
+func SubmitTask(ctx context.Context, ecsAPI EcsClientAPI, input *TaskRunnerConfiguration) (string, error) {
+	response, err := ecsAPI.RunTask(ctx, &ecs.RunTaskInput{
+		Cluster:    &input.Cluster,
+		LaunchType: "FARGATE",
+		Overrides: &types.TaskOverride{
+			ContainerOverrides: []types.ContainerOverride{
+				{
+					Name:    aws.String("migrations-runner"),
+					Command: input.Command,
+				},
+			},
+		},
+		TaskDefinition: &input.TaskDefinitionArn,
+		NetworkConfiguration: &types.NetworkConfiguration{
+			AwsvpcConfiguration: &types.AwsVpcConfiguration{
+				Subnets:        input.SubnetIds,
+				SecurityGroups: input.SecurityGroupIds,
+			},
+		},
+	})
+	if err != nil {
+		return "", err
+	}
+
+	if response.Tasks[0].TaskArn == nil {
+		responseJSON, err := json.Marshal(response)
+		if err != nil {
+			return "", fmt.Errorf("error in unmarshalling response for failed RunTask: %w", err)
+		}
+		return "", fmt.Errorf("ecs:RunTask response contains no TaskArn: %v", string(responseJSON))
+	}
+
+	// this is working on the assumption that only one task is returned
+	return *response.Tasks[0].TaskArn, nil
+}
+
+func WaitForCompletion(ctx context.Context, waiter ecsWaiterAPI, taskArn string) (*ecs.DescribeTasksOutput, error) {
+	cluster := ClusterFromTaskArn(taskArn)
+
+	// TODO: This magic number will be resolved in a future piece of work, not going to refactor this right now
+	maxWaitDuration := 15 * time.Minute //nolint:mnd
+	result, err := waiter.WaitForOutput(ctx, &ecs.DescribeTasksInput{
+		Cluster: aws.String(cluster),
+		Tasks:   []string{taskArn},
+	}, maxWaitDuration)
+
+	// the `DescribeTasksOutput` struct is returned even if there is an error. Counterintuitively, it happens to include failure information
+	// which we may want to surface from the `Failures` struct field
+	if err != nil {
+		return result, err
+	}
+
+	// In a successful scenario, we should have a `tasks` slice with a single element
+	return result, nil
+}
+
+func ClusterFromTaskArn(arn string) string {
+	parts := strings.Split(arn, "/")
+	return parts[len(parts)-2]
+}
+
+func TaskIDFromArn(taskArn string) string {
+	parts := strings.Split(taskArn, "/")
+	return parts[len(parts)-1]
+}
+
+// Describes a single task from a cluster using the native `ecs:DescribeTasks` API.
+// The taskArn is the task of interest to be described
+func DescribeTask(ctx context.Context, ecsClientAPI EcsClientAPI, taskArn string) (types.Task, error) {
+	cluster := ClusterFromTaskArn(taskArn)
+	response, err := ecsClientAPI.DescribeTasks(ctx, &ecs.DescribeTasksInput{
+		Tasks:   []string{taskArn},
+		Cluster: &cluster,
+	})
+
+	if err != nil {
+		return types.Task{}, err
+	}
+
+	// Given the input of `DescribeTasks` we should only be getting one Task back
+	return response.Tasks[0], nil
+}
+
+// Acquires LogStream details for given ECS Task
+func FindLogStreamFromTask(ctx context.Context, ecsClientAPI EcsClientAPI, task types.Task) (LogDetails, error) {
+	response, err := ecsClientAPI.DescribeTaskDefinition(ctx, &ecs.DescribeTaskDefinitionInput{
+		TaskDefinition: task.TaskDefinitionArn,
+	})
+	if err != nil {
+		return LogDetails{}, err
+	}
+
+	if len(response.TaskDefinition.ContainerDefinitions) == 0 {
+		return LogDetails{}, fmt.Errorf("ecs:DescribeTaskDefinition response is missing ContainerDefinitions data: %v", response)
+	}
+
+	container := response.TaskDefinition.ContainerDefinitions[0] // assume first container is the application container
+	logGroupName := container.LogConfiguration.Options["awslogs-group"]
+	//NOTE: Takes the format: prefix-name/container-name/ecs-task-id
+	streamPrefix := container.LogConfiguration.Options["awslogs-stream-prefix"]
+
+	// We need the logGroupName, streamPrefix, and a container name to be able to produce a FindLogStreamOutput in full
+	if logGroupName == "" || streamPrefix == "" {
+		return LogDetails{}, fmt.Errorf("ecs:DescribeTaskDefinition response does not contain required logging configuration: %v", response)
+	}
+
+	return LogDetails{
+		logGroupName:  logGroupName,
+		logStreamName: fmt.Sprintf("%s/%s/%s", streamPrefix, *container.Name, TaskIDFromArn(*task.TaskArn)),
+	}, nil
+}

src/aws/ssm_test.go
@@ -1,0 +1,83 @@
+package aws
+
+import (
+	"context"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"github.com/aws/aws-sdk-go-v2/service/ssm/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type mockGetParameter func(ctx context.Context, params *ssm.GetParameterInput, optFns ...func(*ssm.Options)) (*ssm.GetParameterOutput, error)
+
+func (m mockGetParameter) GetParameter(ctx context.Context, params *ssm.GetParameterInput, optFns ...func(*ssm.Options)) (*ssm.GetParameterOutput, error) {
+	return m(ctx, params, optFns...)
+}
+
+func TestRetrieveConfiguration(t *testing.T) {
+	// JSON strings that would be returned from the SSM parameter store
+	testTaskConfig1 := `{"cluster": "test-cluster","command": ["echo", "hello"],"subnetIds": ["subnet-123456"],"securityGroupIds": ["sg-123456"],"taskDefinitionArn": "arn:aws:ecs:us-west-2:123456789012:task-definition/test-task-1"}`
+	testTaskConfig2 := `{"cluster": "test-cluster","command": ["echo", "byebye"],"subnetIds": ["subnet-654321"],"securityGroupIds": ["sg-654321"],"taskDefinitionArn": "arn:aws:ecs:us-west-2:123456789012:task-definition/test-task-2"}`
+
+	mockedClient1 := mockGetParameter(func(ctx context.Context, params *ssm.GetParameterInput, optFns ...func(*ssm.Options)) (*ssm.GetParameterOutput, error) {
+		return &ssm.GetParameterOutput{
+			Parameter: &types.Parameter{
+				Value: aws.String(testTaskConfig1),
+			},
+		}, nil
+	})
+	mockedClient2 := mockGetParameter(func(ctx context.Context, params *ssm.GetParameterInput, optFns ...func(*ssm.Options)) (*ssm.GetParameterOutput, error) {
+		return &ssm.GetParameterOutput{
+			Parameter: &types.Parameter{
+				Value: aws.String(testTaskConfig2),
+			},
+		}, nil
+	})
+
+	// Running 2 different test cases to validate that the JSON payload unmarshals into the TaskRunnerConfiguration struct
+	tests := []struct {
+		name     string
+		input    string
+		client   ssmAPI
+		expected *TaskRunnerConfiguration
+	}{
+		{
+			name:   "given the parameter name test-parameter-name-1, it should return the configuration",
+			input:  "test-parameter-name-1",
+			client: mockedClient1,
+			expected: &TaskRunnerConfiguration{
+				Cluster:           "test-cluster",
+				Command:           []string{"echo", "hello"},
+				SecurityGroupIds:  []string{"sg-123456"},
+				SubnetIds:         []string{"subnet-123456"},
+				TaskDefinitionArn: "arn:aws:ecs:us-west-2:123456789012:task-definition/test-task-1",
+			},
+		},
+		{
+			name:   "given the parameter name test-parameter-name-2, it should return the configuration",
+			input:  "test-parameter-name-2",
+			client: mockedClient2,
+			expected: &TaskRunnerConfiguration{
+				Cluster:           "test-cluster",
+				Command:           []string{"echo", "byebye"},
+				SecurityGroupIds:  []string{"sg-654321"},
+				SubnetIds:         []string{"subnet-654321"},
+				TaskDefinitionArn: "arn:aws:ecs:us-west-2:123456789012:task-definition/test-task-2",
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := RetrieveConfiguration(context.TODO(), tc.client, tc.input)
+
+			t.Logf("result: %v", result)
+			t.Logf("expected: %v", tc.expected)
+			require.NoError(t, err)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}